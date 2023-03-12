0fe27d4ca20426a113d8aa667df4e7589628d7f6
Seulgie <seulgie.han@dailymotion.com>
2023-03-12 12:13:13 +0100

    Create PersonTester.java

---
+++ Java Fundamentals/lesson2/PersonTester.java
@@ -0,0 +1,10 @@
+public class PersonTester{
+
+    public static void main(String[] args) {
+        Person bob = new Person();
+        Person mike = new Person();
+      
+        System.out.println(bob);
+        System.out.println(mike);
+    }
+}
