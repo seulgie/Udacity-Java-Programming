78c8207bac88efb55701229e5366a54a0026ea6b
Seulgie <seulgie.han@dailymotion.com>
2023-03-12 12:33:08 +0100

    Create Person.java

---
+++ Java Fundamentals/lesson3/Person.java
@@ -0,0 +1,35 @@
+/* Inheritance */
+
+public class Person {
+
+	private String firstName;
+	private String lasName;
+
+	public Person(String firstName, String lasName) {
+		super();
+		this.firstName = firstName;
+		this.lasName = lasName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLasName() {
+		return lasName;
+	}
+
+	public void setLasName(String lasName) {
+		this.lasName = lasName;
+	}
+
+	@Override
+	public String toString() {
+		return "Name:" + firstName + " " + lasName;
+	}
+
+}
