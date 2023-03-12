b89c7f37d200bcee6705224974a1631562b3f3db
Seulgie <seulgie.han@dailymotion.com>
2023-03-12 14:08:40 +0100

    Update Plane.java

--- Java Fundamentals/lesson3/Polymorphism/Plane.java
+++ Java Fundamentals/lesson3/Polymorphism/Plane.java
@@ -6,4 +6,8 @@ public class Plane extends Vehicle {
         // Notice we are passing our arguments into our superclass (Vehicle) constructor
         super("Plane start", "Plane stop", "Plane speed", "Plane direction");
     }
+    
+    public void altitude() {
+        System.out.println("How high are we?");
+    }
 }
