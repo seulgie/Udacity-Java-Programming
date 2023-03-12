299f9f518181a4004adbf1b3bfcf99aad396a1f6
Seulgie <seulgie.han@dailymotion.com>
2023-03-12 12:43:54 +0100

    Create StudentEmployee.java

---
+++ Java Fundamentals/lesson3/StudentEmployee.java
@@ -0,0 +1,31 @@
+public class StudentEmployee extends Student {
+    private String employeeId;
+    private double rateOfPayPerHour;
+  
+    public StudentEmployee(String firstName, String lastName, String studentId, String employeeId, double rateOfPayPerHour) {
+        super(firstName, lastName, studentId);
+        this.employeeId = employeeId;
+        this.rateOfPayPerHour = rateOfPayPerHour;
+    }
+  
+    public String getEmployeeId() {
+        return employeeId;
+    }
+  
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+  
+    public double getRateOfPayPerHour() {
+        return rateOfPayPerHour;
+    }
+  
+    public void setRateOfPayPerHour(double rateOfPayPerHour) {
+        this.rateOfPayPerHour = rateOfPayPerHour;
+    }
+  
+    @Override
+    public String toString() {
+        return super.toString() + " employee ID " + employeeId + " pay " + rateOfPayPerHour;
+    }
+}
