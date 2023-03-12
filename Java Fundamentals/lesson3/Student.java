f95a6ae5acf7b66800c894e1fdf066ad48c007e9
Seulgie <seulgie.han@dailymotion.com>
2023-03-12 12:35:05 +0100

    Create Student.java

---
+++ Java Fundamentals/lesson3/Student.java
@@ -0,0 +1,22 @@
+public class Student extends Person {
+
+	private String studentId;
+
+	public Student(String firstName, String lasName, String studentId) {
+		super(firstName, lasName);
+		this.studentId = studentId;
+	}
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " student ID " + studentId;
+	}
+}
