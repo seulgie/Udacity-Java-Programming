e4ec13567f86fe7af4af075e1afaf2fa2c984151
Seulgie <seulgie.han@dailymotion.com>
2023-03-12 13:50:35 +0100

    Create Plane.java

---
+++ Java Fundamentals/lesson3/Polymorphism/Plane.java
@@ -0,0 +1,9 @@
+/* Polymorphism */
+
+public class Plane extends Vehicle {
+
+    public Plane() {
+        // Notice we are passing our arguments into our superclass (Vehicle) constructor
+        super("Plane start", "Plane stop", "Plane speed", "Plane direction");
+    }
+}
