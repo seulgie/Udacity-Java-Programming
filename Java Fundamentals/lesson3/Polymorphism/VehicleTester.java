cfd5a11f86b048f0ad3a639b95a12a4fc82d1b4c
Seulgie <seulgie.han@dailymotion.com>
2023-03-12 14:25:02 +0100

    Update VehicleTester.java

--- Java Fundamentals/lesson3/Polymorphism/VehicleTester.java
+++ Java Fundamentals/lesson3/Polymorphism/VehicleTester.java
@@ -10,10 +10,29 @@ public class VehicleTester {
         vehicles[0] = new Car();
         vehicles[1] = new Plane();
         vehicles[2] = new Boat();
-      
+        
+        polymorphism(vehicles);
+    }
+    
+    public static void polymorphism(Vehicle[] vehicles) {
+    // Iterate over the array and call the start methods 
         for (int i = 0; i < vehicles.length; i++) {
             Vehicle vehicle = vehicles[i];
             vehicle.speed();
         }
     }
+    
+    public static void coupling() {
+        // Add a Car, Plane and Boat objects to the array
+        Car car = new Car();
+        Plane plane = new Plane();
+        Boat boat = new Boat();
+        
+        car.speed();
+        plane.speed();
+        boat.speed();
+    }
+    
 }
+
+
