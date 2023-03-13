6b06bcd761363fc28502e0c787ffad44f728fe1f
Seulgie <gkstmfrl917@naver.com>
2023-03-13 16:33:09 +0100

    Create genericExample.java

---
+++ Java Fundamentals/lesson5/Generics/genericExample.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsExercise {
+    public static void main(String[] args){
+        ArrayList<Object> variables = new ArrayList<Object>();
+        Double doubleNumber = 1.5;
+        String name = "Sally";
+        int intNumber = 1;
+        char letter = 'a';
+
+        variables.add(doubleNumber);
+        variables.add(name);
+        variables.add(intNumber);
+        variables.add(letter);
+
+        for (Object variable : variables) {
+            GenericsExercise.displayClassName(variable);
+        }
+    }
+    static <T> void displayClassName(T variable) {
+        System.out.println(variable.getClass().getName());
+    }
+
+}
