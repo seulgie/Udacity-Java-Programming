7324e6ddda4777a51fedcfd480420ae2058747c4
Seulgie <seulgie.han@dailymotion.com>
2023-03-13 10:56:03 +0100

    Create PhoneExceptionTester.java

---
+++ Java Fundamentals/lesson4/PhoneExceptionTester.java
@@ -0,0 +1,20 @@
+/* Exceptions */
+
+public class PhoneExceptionTester {
+
+    public static void main() {
+        String[] numbers = new String[] { "123-4567", null, "234-4567", "345-5678" };
+      
+        for(int i = 0; i < numbers.length; i++) {
+            try {
+                System.out.println(new Phone("iPhone", numbers[i]));
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getLocalizedMessage());
+            }
+        }
+    }
+  
+    for (int i = 0; i < numbers.length; i++) {
+        System.out.println(new Phone("iPhone", numbers[i]));
+    }
+}
