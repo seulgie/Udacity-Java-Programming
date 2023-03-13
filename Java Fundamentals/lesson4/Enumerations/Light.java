814b1fc64196ff658f616d078b076442b2c512c6
Seulgie <gkstmfrl917@naver.com>
2023-03-13 11:18:11 +0100

    Create Light.java

---
+++ Java Fundamentals/lesson4/Enumerations/Light.java
@@ -0,0 +1,17 @@
+/* Enumerations */
+
+public class Light {
+  
+    public Light() {
+    }
+  
+    public void changeLight(StopLight currentLight){
+        if (currentLight == StopLight.GREEN) {
+          System.out.println("Green means Go!");
+        } else if (currentLight == StopLight.YELLOW) {
+          System.out.println("Slow down!");
+        } else if (currentLight == StopLight.RED) {
+          System.out.println("Stop!");
+        }
+    }
+}
