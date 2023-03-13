e58af11dc62543060c7e44f70e7e12c1f2674dfc
Seulgie <gkstmfrl917@naver.com>
2023-03-13 17:04:56 +0100

    Update CollectionsExercise.java

--- Java Fundamentals/lesson5/Collections/CollectionsExercise.java
+++ Java Fundamentals/lesson5/Collections/CollectionsExercise.java
@@ -1,15 +1,16 @@
-package collectionsExample;
+import java.util.ArrayList;
 
-import java.util.*;
 public class CollectionsExercise {
 
     public static void main(String[] args){
-        List<String> listOfItems = new LinkedList<String>();
+        //Collections List of Strings
+        ArrayList<String> listOfItems = new LinkedList<String>();
 
         listOfItems.add("Mike");
         listOfItems.add("Bob");
         listOfItems.add("Alice");
 
+        //for each loop
         for (String name : listOfItems){
             System.out.println(name);
         }
